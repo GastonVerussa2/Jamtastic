ec8b27065147a9a5a2d2411c459f9698032078b1
Gaston Verussa <gastonveru@hotmail.com>
2026-04-26 12:48:23 -0300

    vida extra y animacion daño
    Los enemigos tienen uno mas de vida y hay animacion de daño al golpearlos

--- escenas/enemigos/enemigo/enemy.gd
+++ escenas/enemigos/enemigo/enemy.gd
@@ -4,11 +4,13 @@ extends CharacterBody2D
 
 @export var speed: float = 75.0
 @export var sonido_spawn: AudioStreamPlayer2D
+@export var material_personaje_rojo: ShaderMaterial
+@export var sprite: Sprite2D
 
 var damage: int = 10
 var player: Node2D
 var fake_target := Vector2(500,200)
-var health: int = 1
+var health: int = 2
 
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
@@ -27,6 +29,10 @@ func _physics_process(delta):
 	move_and_slide()
 
 func get_hit(damage: int):
+	sprite.material = material_personaje_rojo
+	await get_tree().create_timer(0.1).timeout
+	sprite.material = null
+	await get_tree().create_timer(0.1).timeout
 	health -= damage
 	if(health <= 0):
 		queue_free()
